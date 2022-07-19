a3ed2019773c9b9b0d01c0bb95ac5c3276a28030
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-07-19 17:41:58 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,26 @@
+
+resource "azurerm_key_vault" "wkv" {
+  
+  name                        = var.MD_KV_NAME
+  location                    = var.MD_LOCATION
+  resource_group_name         = var.MD_RG_NAME
+  enabled_for_disk_encryption = true
+  tenant_id                   = var.MD_ARM_TENANT_ID
+  soft_delete_retention_days  = 30
+  purge_protection_enabled    = false # putting as true is not possible to delete secret with TF
+
+  sku_name = "standard"
+  access_policy {
+    tenant_id = var.MD_ARM_TENANT_ID
+    object_id = var.MD_TERRAFORM_SERVICE_ID
+    key_permissions = [
+      "Get",
+    ]
+    secret_permissions = [
+      "Get", "Delete", "Set","Purge", "Recover"
+    ]
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,52 @@
+
+
+variable "MD_KV_NAME" {
+  description = "Key Vault Name"
+  type        = string
+  default     = ""
+
+}
+
+variable "MD_LOCATION" {
+  description = "Key Vault region"
+  type        = string
+  default     = ""
+
+}
+
+variable "MD_RG_NAME" {
+  description = "Key Vault Resource Group"
+  type        = string
+  default     = ""
+
+}
+
+
+variable "MD_PROJECT_NAME" {
+  description = "Prefix foor project name"
+  type        = string
+  default     = ""
+
+}
+
+variable "MD_TERRAFORM_SERVICE_ID" {
+  description = "id og terrafomr-service user"
+  type        = string
+  default     = ""
+}
+
+
+variable "MD_SUBSCRIPTION_PREFIX_SHARED" {
+  description = "Prefix for subscription name"
+  type        = string
+  default     = ""
+
+}
+
+variable "MD_ARM_TENANT_ID" {
+  description = "Tenand Id"
+  type        = string
+  default     = ""
+
+}
+
