5f25d4af9dd0412794a8513663e844cd772886f1
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-07-19 17:53:31 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_key_vault" "wkv" {
-  
+
   name                        = var.MD_KV_NAME
   location                    = var.MD_LOCATION
   resource_group_name         = var.MD_RG_NAME
@@ -8,9 +8,8 @@ resource "azurerm_key_vault" "wkv" {
   tenant_id                   = var.MD_ARM_TENANT_ID
   soft_delete_retention_days  = 30
   purge_protection_enabled    = false # putting as true is not possible to delete secret with TF
+
   sku_name = "standard"
-  tags = var.MD_ALL_TAGS
-  
   access_policy {
     tenant_id = var.MD_ARM_TENANT_ID
     object_id = var.MD_TERRAFORM_SERVICE_ID
@@ -18,7 +17,7 @@ resource "azurerm_key_vault" "wkv" {
       "Get",
     ]
     secret_permissions = [
-      "Get", "Delete", "Set","Purge", "Recover"
+      "Get", "Delete", "Set", "Purge", "Recover"
     ]
     storage_permissions = [
       "Get",
